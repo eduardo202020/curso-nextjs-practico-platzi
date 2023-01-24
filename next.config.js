31787c32667835f820b5a88259060ca6540e9483
Eduardo26 <jguevaral@uni.pe>
2023-01-24 15:38:41 -0500

    fix sin pwa

--- next.config.js
+++ next.config.js
@@ -18,10 +18,11 @@ const nextConfig = {
   },
 };
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-});
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   register: true,
+// });
 
-module.exports = withPWA(nextConfig);
+// module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
